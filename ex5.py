845dc9edc735ad96fc6fd3ed256e6ae5f6ca79ee
Gaurav Kumar <kumar.gauravnitk@gmail.com>
2017-10-08 23:37:03 +0200

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,3 +1,13 @@
+"""
+Take two lists, say for example these two:
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+  b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+and write a program that returns a list that contains only the elements that are 
+common between the lists (without duplicates). 
+Make sure your program works on two lists of different sizes.
+"""
+
+
 import random
 randoms_first = random.sample(xrange(100), 10)
 randoms_second = random.sample(xrange(100), 20)
@@ -18,3 +28,4 @@ if len(result_list) > 0:
     # result_list = [2, 2, 3, 3, 4, 5, 6]
     # print(result_list)
     print (list(set(result_list)))
+
